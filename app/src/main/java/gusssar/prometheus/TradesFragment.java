d91c37580057e351d340afed3ea27090030d2850
gusssar <dassler47@gmail.com>
2018-06-07 17:53:15 +0300

    need listview for array

--- app/src/main/java/gusssar/prometheus/TradesFragment.java
+++ app/src/main/java/gusssar/prometheus/TradesFragment.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 
@@ -40,14 +42,47 @@ public class TradesFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+            }
+          }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trades, container, false);
+
+        View view =inflater.inflate(R.layout.fragment_trades, container, false);
+        ListView listView = (ListView)view.findViewById(R.id.list_trades);
+        String[] pairList = getResources().getStringArray(R.array.pairlist);
+        String[] pairLink = getResources().getStringArray(R.array.pairlink);
+            //ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_2,pairList);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item,pairList);
+            listView.setAdapter(adapter);
+        return view;
     }
+
+//   private class CatAdapter extends ArrayAdapter<Cat> {
+
+//       public CatAdapter(Context context) {
+//           super(context, android.R.layout.simple_list_item_2, cats);
+//       }
+
+//       @Override
+//       public View getView(int position, View convertView, ViewGroup parent) {
+//           Cat cat = getItem(position);
+
+//           if (convertView == null) {
+//               convertView = LayoutInflater.from(getContext())
+//                       .inflate(android.R.layout.simple_list_item_2, null);
+//           }
+//           ((TextView) convertView.findViewById(android.R.id.text1))
+//                   .setText(cat.name);
+//           ((TextView) convertView.findViewById(android.R.id.text2))
+//                   .setText(cat.gender);
+//           return convertView;
+//       }
+//   }
+
+
+
 
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -76,3 +111,4 @@ public class TradesFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 }
+
